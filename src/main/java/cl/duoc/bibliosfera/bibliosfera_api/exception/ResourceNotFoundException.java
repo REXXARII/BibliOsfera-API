bf4b2e32f18c4650f908066a71a4f8efa24d4127
REXARII <158285393+REXXARII@users.noreply.github.com>
2025-07-16 04:03:39 -0400

    exception

---
+++ src/main/java/cl/duoc/bibliosfera/bibliosfera_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package cl.duoc.bibliosfera.bibliosfera_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
